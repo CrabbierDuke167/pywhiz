aa24b7a900351bd5adcb1b6ccd2f105c6ec3004f
CrabbierDuke167 <vijidaison2@gmail.com>
2026-08-16 22:38:33 +0530

    update project

--- src/pywhiz/__init__.py
+++ src/pywhiz/__init__.py
@@ -2,7 +2,7 @@
 pywhiz - Fast, effortless, and friendly Python helper functions.
 """
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 from .string_utils import (
     v_count,
@@ -17,6 +17,14 @@ from .text_file_utils import (
     txt_w_pos,
 )
 
+from .list_utils import (
+    unique,
+    duplicates,
+    compact,
+    shuffle,
+    sample,
+    sample_one,
+)
 __all__ = [
     "v_count",
     "c_count",
@@ -25,4 +33,10 @@ __all__ = [
     "even_count",
     "txt_wc",
     "txt_w_pos",
+    "unique",
+    "duplicates",
+    "compact",
+    "shuffle",
+    "sample",
+    "sample_one",
 ]
