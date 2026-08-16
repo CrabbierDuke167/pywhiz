b7c54f6895055b6bc60168e42f7742d22bf684f2
CrabbierDuke167 <vijidaison2@gmail.com>
2026-08-16 13:41:01 +0530

    release v0.1.3

--- src/pywhiz/__init__.py
+++ src/pywhiz/__init__.py
@@ -2,16 +2,20 @@
 pywhiz - Fast, effortless, and friendly Python helper functions.
 """
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from .string_utils import (
     v_count,
     c_count,
     w_count,
+    odd_count,
+    even_count,
 )
 
 __all__ = [
     "v_count",
     "c_count",
     "w_count",
+    "odd_count",
+    "even_count",
 ]
