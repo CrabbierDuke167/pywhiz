cb12493c0fb8c9c34b0f3b4e71f30f7fa81ae83e
nihal_sam <nihalsam2009@gmail.com>
2026-08-16 20:23:37 +0530

    Remove unused countf and positionf functions
    Removed unused functions countf and positionf from string_utils.py.

--- src/pywhiz/string_utils.py
+++ src/pywhiz/string_utils.py
@@ -46,24 +46,4 @@ def even_count(num):
             count+=1
     return count
 
-def countf(obj,file):
-    with open(file, 'r') as f:
-        n=0
-        for line in f:
-            words = line.split()
-            for word in words:
-                if obj == word:
-                    n+=1
-    return n
-
-def positionf(word,file):
-    with open(file, 'r') as f:
-        text = f.readlines()
-        for y, line in enumerate(text, start=1):
-           for x, w in enumerate(line.split(), start=1):
-               if w == word:
-                 return (x, y)
-       
-    return None
-
 
